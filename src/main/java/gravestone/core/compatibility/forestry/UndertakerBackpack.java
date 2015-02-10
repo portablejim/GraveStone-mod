c8aaca8ea25e1734155ab38dd5b0745a2cd16b1f
James McKay <dcseee-github@yahoo.com.au>
2015-02-11 03:55:27 +1100

    Update UndetakerBackpack for forestry

--- src/main/java/gravestone/core/compatibility/forestry/UndertakerBackpack.java
+++ src/main/java/gravestone/core/compatibility/forestry/UndertakerBackpack.java
@@ -75,7 +75,20 @@ public class UndertakerBackpack implements IBackpackDefinition {
     }
 
     @Override
+    public void addValidItems(List<ItemStack> validItems) {
+        for(ItemStack item : validItems) {
+            addValidItem(item);
+        }
+    }
+
+    @Override
     public boolean isValidItem(EntityPlayer player, ItemStack itemstack) {
+        return allowedBlocks.contains(Block.getBlockFromItem(itemstack.getItem())) ||
+                allowedItems.contains(itemstack.getItem());
+    }
+
+    @Override
+    public boolean isValidItem(ItemStack itemstack) {
         return allowedBlocks.contains(Block.getBlockFromItem(itemstack.getItem())) ||
                 allowedItems.contains(itemstack.getItem());
     }
