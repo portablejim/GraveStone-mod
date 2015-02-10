d9e685fc95b44e1e55c0a93874aa2137d205b26c
James McKay <dcseee-github@yahoo.com.au>
2015-02-11 03:54:33 +1100

    Port to gradle

---
+++ src/api/java/micdoodle8/mods/galacticraft/api/inventory/AccessInventoryGC.java
@@ -0,0 +1,17 @@
+package micdoodle8.mods.galacticraft.api.inventory;
+
+import net.minecraft.entity.player.EntityPlayerMP;
+
+/**
+ * A static method for other mods to access the Galacticraft
+ * extended inventory.
+ * 
+ * Call: AccessInventoryGC.getGCInventoryForPlayer(player)
+ */
+public class AccessInventoryGC
+{
+	public static IInventoryGC getGCInventoryForPlayer(EntityPlayerMP player)
+	{
+        return null;
+	}
+}
